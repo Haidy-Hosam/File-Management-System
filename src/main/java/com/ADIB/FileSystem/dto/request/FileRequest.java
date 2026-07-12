210d3cefe64223e7784d7ff8a76105ae3d76e651
Gannah211 <gannahmohamed522@gmail.com>
2026-07-12 10:41:46 +0300

    add file entity and dtos

---
+++ src/main/java/com/ADIB/FileSystem/dto/request/FileRequest.java
@@ -0,0 +1,19 @@
+package com.ADIB.FileSystem.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileRequest {
+    private MultipartFile file;
+    private Long department_id;
+    @NotBlank(message = "file status is required.")
+    private String status;
+}
